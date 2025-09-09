42feee942ffcabd72e8b2ca664fe778810917ab9
Sahawit <shwithyesemaw8@gmail.com>
2025-09-09 10:13:04 +0700

    edit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css'
 import CounterScore from './component/CounterScore';
-import CounterInc from './component/Counterinc';
+import CounterInc from './component/CounterInc';
 import CounterDec from './component/CounterDec';
 import TodoList from './component/TodoList';
 import CourseForm from './component/CourseForm';
